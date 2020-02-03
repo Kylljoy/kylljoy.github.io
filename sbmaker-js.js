9369c6199de8b7191108deb229b53f846bac63c8
Kyle Heller <kyleeheller@gmail.com>
2020-02-03 10:17:45 -0800

    SBCreator Skills partially started

--- sbmaker-js.js
+++ sbmaker-js.js
@@ -13,6 +13,101 @@ var phspoints=0;
 var cogpoints=0;
 var corpoints=0;
 var chrpoints=0;
+var sk_phs=0;
+var sk_cog=0;
+var sk_cor=0;
+var sk_chr=0;
+var med_init=0;
+var sur_init=0;
+var rep_init=0;
+var inv_init=0;
+var for_init=0;
+var str_init=0;
+var sle_init=0;
+var hid_init=0;
+var spe_init=0;
+var spc_init=0;
+
+
+var init_sks={"med":0,"sur":0,"inv":0,"rep":0,"for":0,"str":0,"hid":0,"sle":0,"hid":0,"spe":0,"spc":0};
+var gen_sks=["med","sur","inv","rep","for","str","sle","hid","spe","spc"];
+
+var tg_sks=[];
+
+function inskup(skillname){
+	var skillval = 0;
+	switch(skillname){
+			case "med":
+			case "sur":
+			case "inv":
+			case "rep":
+			skillval=sk_cog;
+			break;
+			case "for":
+			case "str":
+			skillval=sk_phs;
+			break;
+			case "sle":
+			case "hid":
+			skillval=sk_cor;
+			break;
+			case "spc":
+			case "spe":
+			skillval=sk_chr;
+			break;
+	}
+	if(skillval>0){
+		switch(skillname){
+			case "med":
+			case "sur":
+			case "inv":
+			case "rep":
+			sk_cog--;
+			break;
+			case "for":
+			case "str":
+			sk_phs--;
+			break;
+			case "sle":
+			case "hid":
+			sk_cor--;
+			break;
+			case "spc":
+			case "spe":
+			sk_chr--;
+			break;
+	}
+	init_sks[skillname]++;
+	}
+	updatetags();
+}
+
+function inskdo(skillname){
+	if(init_sks[skillname]>0){
+		switch(skillname){
+			case "med":
+			case "sur":
+			case "inv":
+			case "rep":
+			sk_cog++;
+			break;
+			case "for":
+			case "str":
+			sk_phs++;
+			break;
+			case "sle":
+			case "hid":
+			sk_cor++;
+			break;
+			case "spc":
+			case "spe":
+			sk_chr++;
+			break;
+	}
+	init_sks[skillname]--;
+	}
+	updatetags();
+}
 
 function updatename(){
 		if(document.getElementById("name").value=="God"){
@@ -103,6 +198,7 @@ function updateclass(){
 			class_health=6;
 	}
 		updatetags();
+		updatenatskills();
 	}
 
 
@@ -149,8 +245,26 @@ function updaterace(){
 		break;
 		
 	}
-	
 		updatetags();
+		updatenatskills();
+}
+
+function updatenatskills(){
+	sk_phs=document.getElementById("phs").innerHTML;
+	sk_cog=document.getElementById("cog").innerHTML;
+	sk_cor=document.getElementById("cor").innerHTML;
+	sk_chr=document.getElementById("chr").innerHTML;
+	med_init=0;
+	sur_init=0;
+	rep_init=0;
+	inv_init=0;
+	for_init=0;
+	str_init=0;
+	sle_init=0;
+	hid_init=0;
+	spe_init=0;
+	spc_init=0;
+	
 }
 
 function updatetags(){
@@ -169,10 +283,30 @@ function updatetags(){
 	document.getElementById("corf").value=Math.max(class_cor+race_cor,0)+corpoints;
 	document.getElementById("chrf").value=Math.max(class_chr+race_chr,0)+chrpoints;
 	document.getElementById("healf").value=Math.max(class_health+race_health,0)+(2*(document.getElementById("level").value-1));
+	for(i=0;i<gen_sks.length;i++){
+		document.getElementById("nat"+gen_sks[i]).innerHTML=init_sks[gen_sks[i]]*(1+tg_sks.count("med"));
+	}
+
+	document.getElementById("sk_phs").innerHTML=sk_phs;
+	document.getElementById("sk_cor").innerHTML=sk_cor;
+	document.getElementById("sk_chr").innerHTML=sk_chr;
+	document.getElementById("sk_cog").innerHTML=sk_cog;
+
+
+
+
 }
 
+Object.defineProperties(Array.prototype, {
+    count: {
+        value: function(value) {
+            return this.filter(x => x==value).length;
+        }
+    }
+});
+
 function allofthem(){
-		updatelevel();
+	updatelevel();
 	updateclass();
 	updaterace();
 	updatetags();
